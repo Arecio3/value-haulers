81b8b93710ba1a8f2328be5cabdcbd669a9af632
Arecio Canton <cuba289@gmail.com>
2021-10-27 07:01:59 -0400

    added animations

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ReviewSection from './Components/ReviewSection';
 import Header from "./Components/Header";
 import Contact from "./Components/Contact";
 import { OuterLayout } from "./styles/Layouts";
+import { Fade } from "react-awesome-reveal";
 
 function App() {
   return (
@@ -11,9 +12,15 @@ function App() {
       <Header />
       <OuterLayout>
         <MainStyled>
+          <Fade duration={1000} direction="right">
           <CardSection />
-          <ReviewSection />
-          <Contact/>
+          </Fade>
+          <Fade  duration={1000}>
+          <ReviewSection/>
+          </Fade>
+          <Fade duration={1000} direction="left">
+          <Contact />
+          </Fade>
         </MainStyled>
       </OuterLayout>
     </div>
